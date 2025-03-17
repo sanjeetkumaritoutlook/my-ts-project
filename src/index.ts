8da599fd3ab9ff8a88e75de3f87ee7599f6f7ad7
unknown <sanjeetkumarit@outlook.com>
2025-03-17 09:22:35 +0530

    installed package from AWs CodeArtifact

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import { fetchData } from "./services/api";
 import logger from "./utils/logger";
 import { printPerson, User, Admin } from "./types/personUtils";
 import { printId } from "./types/utils";
+//installed from private registry in  AWS CodeArtifact, registry has it own domain, not npm
+const { greet } = require('@my-scope/hello-package');
+
+console.log(greet('Sanjeet'));
 
 console.log("Addition:", add(5, 10));
 console.log("Multiplication:", multiply(3, 4));
