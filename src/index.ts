e8afa59402f849754d4a241d5440ad6f85be283d
unknown <sanjeetkumarit@outlook.com>
2025-03-17 09:49:58 +0530

    updated scoped package

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,12 @@ import logger from "./utils/logger";
 import { printPerson, User, Admin } from "./types/personUtils";
 import { printId } from "./types/utils";
 //installed from private registry in  AWS CodeArtifact, registry has it own domain, not npm
-const { greet } = require('@my-scope/hello-package');
+const { greet ,farewell } = require('@my-scope/hello-package');
 
 console.log(greet('Sanjeet'));
+console.log(greet('Sanjeet', 'hi')); 
+console.log(greet('Sanjeet', 'es')); 
+console.log(farewell('Sanjeet')); 
 
 console.log("Addition:", add(5, 10));
 console.log("Multiplication:", multiply(3, 4));
